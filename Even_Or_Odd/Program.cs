c1584fc607eb874c9faa2118abea9aea16e842a6
Srsanchxz <79011986+Srsanchxz@users.noreply.github.com>
2023-07-25 20:13:06 -0600

    añadiendo par o impar
    añadiendo el ejercicio numero uno, con sus respectivas valiidaciones.

--- Even_Or_Odd/Program.cs
+++ Even_Or_Odd/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Programa que determina si un numero es par o impar:");
+
+        int numero = 0;
+        bool esNumeroValido = false;
+
+        // realizando valiidacion (que el numero sea entero y positivo)
+        while (!esNumeroValido)
+        {
+            string input = Console.ReadLine(); //leyendo por consola
+
+            if (int.TryParse(input, out numero) && numero >= 0)
+            {
+                esNumeroValido = true;
+            }
+            else
+            {
+                Console.WriteLine("Por favor, ingrese un número válido y positivo:");
+            }
+        }
+
+        // analizando si es par o impar 
+        if (EsPar(numero))
+        {
+            Console.WriteLine($"El número {numero} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {numero} es impar.");
+        }
+    }
+
+    // Función para determinar si un número es par o impar
+    static bool EsPar(int numero)
+    {
+        return numero % 2 == 0;
+    }
+}
